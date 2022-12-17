db15678906ae67c5ab77e11319ea050366b8d97f
liyiooooo <usa831119@gmail.com>
2022-12-17 12:01:52 +0800

    add test.js

--- crawler/test.js
+++ crawler/test.js
@@ -1,14 +1,24 @@
 // install mysql2
 
 const mysql2 = require('mysql2/promise');
+//npm i dotenv
+//2方法
+require('dotenv').config();
 
 (async () => {
   const connection = await mysql2.createConnection({
-    host: '127.0.0.1',
-    port: 3306,
-    user: 'admin',
-    password: '',
-    database: 'stock_mfee31',
+    // host: '127.0.0.1',
+    // port: 3306,
+    // user: 'admin',
+    // password: '',
+    // database: 'stock_mfee31',
+    
+    //2方法
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME,
   });
 
   // simple query
